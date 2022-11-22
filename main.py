fd40c2b7de6c02b97a9e2793d01fa8a7259dc520
mujuan12 <90755758+mujuan12@users.noreply.github.com>
2022-11-22 19:44:30 +0800

    Update main.py

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ message = '''
 Headers = {
     'Authorization': '',
     'Cookie': '',
-    'Referer': 'http://dekt.jiangnan.edu.cn/admin/index.html',
+    'Referer': 'https://win.9xueqi.com/#/home',
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.72 Safari/537.36'
 }
 
